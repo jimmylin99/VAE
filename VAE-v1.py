97a616247633b96e96c03d33e98c8eda01b8d5a3
LIN-PC <jimmylinh@icloud.com>
2020-03-11 22:40:29 +0800

    Initial

--- VAE-v1.py
+++ VAE-v1.py
@@ -2,21 +2,31 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from keras.layers import Dense, Input, concatenate
-from keras.layers import Conv2D, Flatten, Lambda
-from keras.layers import Reshape, Conv2DTranspose
-from keras.models import Model
-from keras.datasets import mnist
-from keras.losses import mse, binary_crossentropy
-from keras.utils import plot_model
-from keras import backend as K
+
+from tensorflow.keras.layers import Dense, Input, concatenate
+from tensorflow.keras.layers import Conv2D, Flatten, Lambda
+from tensorflow.keras.layers import Reshape, Conv2DTranspose
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import mse, binary_crossentropy
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras import backend as K
 
 import math
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
 import os
 
+
+parser = argparse.ArgumentParser()
+help_ = "Load h5 model trained weights"
+parser.add_argument("-w", "--weights", help=help_)
+help_ = "Use mse loss instead of binary cross entropy (default)"
+parser.add_argument("-m", "--mse", help=help_, action='store_true')
+parser.add_argument("-e", "--epoch", help="epoch", default=2, type=int)
+args = parser.parse_args()
 
 # reparameterization trick
 # instead of sampling from Q(z|X), sample eps = N(0,I)
@@ -39,68 +49,6 @@ def sampling(args):
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 
-def plot_results(models,
-                 data,
-                 batch_size=128,
-                 model_name="vae_mnist"):
-    """Plots labels and MNIST digits as function of 2-dim latent vector
-
-    # Arguments
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    """
-
-    encoder, decoder = models
-    x_test, y_test = data
-    os.makedirs(model_name, exist_ok=True)
-
-    filename = os.path.join(model_name, "vae_mean.png")
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.savefig(filename)
-    plt.show()
-
-    filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 30x30 2D manifold of digits
-    n = 30
-    digit_size = 28
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-4, 4, n)
-    grid_y = np.linspace(-4, 4, n)[::-1]
-
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
-
-    plt.figure(figsize=(10, 10))
-    start_range = digit_size // 2
-    end_range = n * digit_size + start_range + 1
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap='Greys_r')
-    plt.savefig(filename)
-    plt.show()
-
-
 # DUCKIETOWN dataset
 dataset_file = np.load('duckie_img.npz')
 dataset = dataset_file["arr_0"]
@@ -120,7 +68,7 @@ batch_size = 36
 kernel_size = 5
 filters = 16
 latent_dim = 50
-epochs = 80
+epochs = args.epoch
 strides = (2, 2)
 
 # VAE model = encoder + decoder
@@ -209,102 +157,97 @@ print(encoder(inputs)[3])
 outputs = decoder(merged_model([encoder(inputs)[2], encoder(inputs)[3]]))
 vae = Model(inputs, outputs, name='vae')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    help_ = "Load h5 model trained weights"
-    parser.add_argument("-w", "--weights", help=help_)
-    help_ = "Use mse loss instead of binary cross entropy (default)"
-    parser.add_argument("-m", "--mse", help=help_, action='store_true')
-    args = parser.parse_args()
-    models = (encoder, decoder)
-    data = (x_test, y_test)
 
-    # VAE loss = mse_loss or xent_loss + kl_loss
-    #if args.mse:
+models = (encoder, decoder)
+data = (x_test, y_test)
+
+# VAE loss = mse_loss or xent_loss + kl_loss
+if args.mse:
     reconstruction_loss = mse(K.flatten(input_fifth),
-                              K.flatten(outputs))
-    #else:
-    #    reconstruction_loss = binary_crossentropy(K.flatten(inputs),
-    #                                              K.flatten(outputs))
+                          K.flatten(outputs))
+else:
+    reconstruction_loss = binary_crossentropy(K.flatten(input_fifth),
+                                             K.flatten(outputs))
 
-    reconstruction_loss *= image_size[0] * image_size[1]
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = K.mean(reconstruction_loss + kl_loss)
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='adam')
-    vae.summary()
-    plot_model(vae, to_file='vae_cnn.png', show_shapes=True)
+reconstruction_loss *= image_size[0] * image_size[1]
+kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+kl_loss = K.sum(kl_loss, axis=-1)
+kl_loss *= -0.5
+vae_loss = K.mean(reconstruction_loss + kl_loss)
+vae.add_loss(vae_loss)
+vae.compile(optimizer='adam')
+vae.summary()
+plot_model(vae, to_file='vae_cnn.png', show_shapes=True)
 
-    from keras.callbacks import TensorBoard
-    import datetime
-    log_dir = os.path.join(
-        "logs",
-        "fit",
-        datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),
-    )
-    tbCallBack = TensorBoard(log_dir=log_dir,  # log 目录
-                             histogram_freq=0,  # 按照何等频率（epoch）来计算直方图，0为不计算
-                             #                  batch_size=32,     # 用多大量的数据计算直方图
-                             write_graph=True,  # 是否存储网络结构图
-                             write_grads=True,  # 是否可视化梯度直方图
-                             write_images=True,  # 是否可视化参数
-                             embeddings_freq=0,
-                             embeddings_layer_names=None,
-                             embeddings_metadata=None,
-                             # update_freq=100
-                             )
-    from keras.callbacks import ModelCheckpoint
-    cpCallBack = ModelCheckpoint("weights.hdf5",
-                                 monitor='val_loss',
-                                 verbose=0,
-                                 save_best_only=True,
-                                 save_weights_only=False,
-                                 mode='min',
-                                 period=1)
-    from keras.callbacks import EarlyStopping
-    esCallBack = EarlyStopping(monitor='val_loss',
-                               min_delta=0,
-                               patience=6,
-                               verbose=0,
-                               mode='auto',
-                               baseline=None,
-                               restore_best_weights=False)
-    from keras.models import load_model
-    if args.weights:
-        # vae.load_weights(args.weights)
-        vae = load_model('vae_cnn_model.h5')
-        vae.fit(x_train,
-                epochs=epochs,
-                batch_size=batch_size,
-                validation_data=(x_test, None),
-                callbacks=[tbCallBack, cpCallBack, esCallBack])
-        vae.save_weights('vae_cnn_duckie.h5')
-        vae.save('vae_cnn_model.h5')
-    else:
-        # train the autoencoder
-        vae.fit(x_train,
-                epochs=epochs,
-                batch_size=batch_size,
-                validation_data=(x_test, None),
-                callbacks=[tbCallBack, cpCallBack, esCallBack])
-        vae.save_weights('vae_cnn_duckie.h5')
-        vae.save('vae_cnn_model.h5')
+from tensorflow.keras.callbacks import TensorBoard
+import datetime
+log_dir = os.path.join(
+    "logs",
+    "fit",
+    datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),
+)
+tbCallBack = TensorBoard(log_dir=log_dir,  # log 目录
+                         histogram_freq=0,  # 按照何等频率（epoch）来计算直方图，0为不计算
+                         #                  batch_size=32,     # 用多大量的数据计算直方图
+                         write_graph=True,  # 是否存储网络结构图
+                         write_grads=True,  # 是否可视化梯度直方图
+                         write_images=True,  # 是否可视化参数
+                         embeddings_freq=0,
+                         embeddings_layer_names=None,
+                         embeddings_metadata=None,
+                         # update_freq=100
+                         )
+from tensorflow.keras.callbacks import ModelCheckpoint
+cpCallBack = ModelCheckpoint("weights.tf",
+                             monitor='val_loss',
+                             verbose=0,
+                             save_best_only=True,
+                             save_weights_only=False,
+                             mode='min',
+                             period=1)
+from tensorflow.keras.callbacks import EarlyStopping
+esCallBack = EarlyStopping(monitor='val_loss',
+                           min_delta=0,
+                           patience=6,
+                           verbose=0,
+                           mode='auto',
+                           baseline=None,
+                           restore_best_weights=False)
+from tensorflow.keras.models import load_model
+if args.weights:
+    # vae.load_weights(args.weights)
+    vae = load_model('vae_cnn_model.tf')
+    vae.fit(x_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_test, None),
+            callbacks=[tbCallBack, cpCallBack, esCallBack])
+    vae.save_weights('vae_cnn_duckie.tf')
+    vae.save('vae_cnn_model.tf')
+else:
+    # train the autoencoder
+    vae.fit(x_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_test, None),
+            callbacks=[tbCallBack, cpCallBack, esCallBack])
+    vae.save_weights('vae_cnn_duckie.tf')
+    vae.save('vae_cnn_model.tf')
 
-    # plot_results(models, data, batch_size=batch_size, model_name="vae_cnn")
-    outputImg = vae.predict(x_test, batch_size=batch_size)
-    figure = plt.figure()
-    _width = 480 * 5 + 1
-    ax = figure.add_subplot(221)
-    ax.imshow(np.reshape(x_test[0], [_width, 640]), cmap=plt.cm.gray)
-    ax = figure.add_subplot(222)
-    ax.imshow(np.reshape(outputImg[0], [480, 640]), cmap=plt.cm.gray)
-    ax = figure.add_subplot(223)
-    ax.imshow(np.reshape(x_train[0], [_width, 640]), cmap=plt.cm.gray)
-    ax = figure.add_subplot(224)
-    ax.imshow(np.reshape(vae.predict(x_train, batch_size=batch_size)[0], [480, 640]),
-              cmap=plt.cm.gray)
-    plt.show()
-    # figure = plt.figure()
-    # plt.imshow(figure, cmap='Greys_r')
+# plot_results(models, data, batch_size=batch_size, model_name="vae_cnn")
+outputImg = vae.predict(x_test, batch_size=batch_size)
+figure = plt.figure()
+_width = 480 * 5 + 1
+ax = figure.add_subplot(221)
+ax.imshow(np.reshape(x_test[0], [_width, 640]), cmap=plt.cm.gray)
+ax = figure.add_subplot(222)
+ax.imshow(np.reshape(outputImg[0], [480, 640]), cmap=plt.cm.gray)
+ax = figure.add_subplot(223)
+ax.imshow(np.reshape(x_train[0], [_width, 640]), cmap=plt.cm.gray)
+ax = figure.add_subplot(224)
+ax.imshow(np.reshape(vae.predict(x_train, batch_size=batch_size)[0], [480, 640]),
+          cmap=plt.cm.gray)
+# plt.show()
+plt.savefig('showSampledResults.png')
+# figure = plt.figure()
+# plt.imshow(figure, cmap='Greys_r')
